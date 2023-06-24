5cde138cdc184dc8c8c7604e32cb599d77ca1559
Nicholas J Dunn <nicholasduunn@gmail.com>
2023-06-24 09:01:17 -0700

    added realm context to app & realm file for config & screens folder

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
+import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
+import RealmContext from './realm';
+
+const {RealmProvider} = RealmContext;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Verve</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RealmProvider>
+      <View style={styles.container}>
+        <Text>Verve</Text>
+        <StatusBar style="auto" />
+      </View>
+    </RealmProvider>
   );
 }
 
---
+++ realm.ts
@@ -0,0 +1,9 @@
+import {createRealmContext} from '@realm/react';
+import {Workout} from './models/workout';
+import {Meal} from './models/meals';
+
+const config = {
+  // schema: [Workout, Meal],
+  schema: [Workout],
+};
+export default createRealmContext(config);
