540611729fad4a50f4452f6cf830ee601f1a9626
Nicholas J Dunn <nicholasduunn@gmail.com>
2023-06-24 13:44:45 -0700

    removed realm dependencies and files

--- App.tsx
+++ App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
 import {StatusBar} from 'expo-status-bar';
 import {Text, View} from 'react-native';
-import RealmContext from './realm';
-
-const {RealmProvider} = RealmContext;
 
 export default function App() {
   return (
-    <RealmProvider>
-      <View className="flex-1 items-center justify-center bg-black">
-        <Text className="text-slate-400">NICK TOKA REACT APP</Text>
-        <StatusBar style="auto" />
-      </View>
-    </RealmProvider>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-slate-400">NICK TOKA REACT APP</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
--- realm.ts
+++
@@ -1,9 +0,0 @@
-import {Realm, createRealmContext} from '@realm/react';
-import {Workout} from './models/workout';
-import {Meal} from './models/meals';
-
-const config = {
-  // schema: [Workout, Meal],
-  schema: [Workout],
-};
-export default createRealmContext(config);
