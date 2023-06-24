33778b708a4069c41f35fa45bb7da85e21ca792c
Nicholas J Dunn <nicholasduunn@gmail.com>
2023-06-24 13:39:44 -0700

    added tailwind to project

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StatusBar} from 'expo-status-bar';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
 import RealmContext from './realm';
 
 const {RealmProvider} = RealmContext;
@@ -8,19 +8,19 @@ const {RealmProvider} = RealmContext;
 export default function App() {
   return (
     <RealmProvider>
-      <View style={styles.container}>
-        <Text>Verve</Text>
+      <View className="flex-1 items-center justify-center bg-black">
+        <Text className="text-slate-400">NICK TOKA REACT APP</Text>
         <StatusBar style="auto" />
       </View>
     </RealmProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
