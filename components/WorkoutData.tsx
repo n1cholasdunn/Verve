750df10800b47aff259ded1e89248487f84ebdd2
Tornike <tornike_kutaisi@yahoo.ca>
2023-06-30 12:07:33 -0400

    added remove functionality to workout cards

--- components/WorkoutData.tsx
+++ components/WorkoutData.tsx
@@ -5,11 +5,13 @@ import {
   Text,
   View,
   StyleSheet,
+  Pressable,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
-import {collection, onSnapshot} from '@firebase/firestore';
+import {collection, deleteDoc, doc, onSnapshot} from '@firebase/firestore';
 import {db} from '../firebaseConfig';
 import {Workout} from '../types/workout';
+import {AntDesign} from '@expo/vector-icons';
 
 const WorkoutData = ({day, user}) => {
   const [workouts, setWorkouts] = useState<Workout[]>([]);
@@ -30,16 +32,31 @@ const WorkoutData = ({day, user}) => {
     });
   }, []);
 
+  const handleDelete = async workout => {
+    try {
+      await deleteDoc(doc(db, 'workout-test', workout));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const renderWorkouts = ({item}) => (
-    <View className="p-5 mb-10 ml-3 mr-3 h-180 w-300 bg-[#1E1E1E] border rounded-md">
-      <Text className="text-2xl text-[#BB86FC]">{item.name}</Text>
-      <Text className="text-lg text-[#606368]">Muscle: {item.muscle}</Text>
-      <Text className="text-lg text-[#606368]">Reps: {item.reps}</Text>
-      <Text className="text-lg text-[#606368]">Sets: {item.sets}</Text>
-      <Text className="text-lg text-[#606368]">Weight: {item.weight}lbs</Text>
-      <Text className="text-lg text-[#606368]">
-        Date: {item.date.toString()}
-      </Text>
+    <View className="p-5 mb-10 ml-3 mr-3 h-180 w-300 bg-[#1E1E1E] border rounded-md flex-row">
+      <View>
+        <Text className="text-2xl text-[#BB86FC]">{item.name}</Text>
+        <Text className="text-lg text-[#606368]">Muscle: {item.muscle}</Text>
+        <Text className="text-lg text-[#606368]">Reps: {item.reps}</Text>
+        <Text className="text-lg text-[#606368]">Sets: {item.sets}</Text>
+        <Text className="text-lg text-[#606368]">Weight: {item.weight}lbs</Text>
+        <Text className="text-lg text-[#606368]">
+          Date: {item.date.toString()}
+        </Text>
+      </View>
+      <View className="ml-[135px]">
+        <Pressable onPress={() => handleDelete(item.id)}>
+          <AntDesign name="delete" size={24} color="#606368" />
+        </Pressable>
+      </View>
     </View>
   );
 
